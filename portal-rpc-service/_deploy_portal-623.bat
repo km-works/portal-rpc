dd7b8ce6065f815d530b3bc47d42d0852f279562
Christian P. Lerch <christian.p.lerch [at] gmail.com>
2016-02-28 09:42:24 +0100

    reconfigure .gitignore

--- portal-rpc-service/_deploy_portal-623.bat
+++
@@ -1,3 +0,0 @@
-copy target\portal-rpc-service-6.2.3.*.jar X:\liferay\portal-623-ga4\tomcat-7.0.42\lib\ext
-
-pause
--- rpc-server-hook/_pull_from-sdk-623.bat
+++
@@ -1,4 +0,0 @@
-set TGT=E:\devtools\liferay-portal\plugins-sdk-623-ga4\hooks
-set SELF=rpc-server-hook
-robocopy %TGT%\%SELF% . /MIR
-pause
